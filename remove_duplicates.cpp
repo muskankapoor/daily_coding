d689a17b5063f0d0cc901c4bec82fff76bea2802
muskankapoor <muskankapoor016@gmail.com>
2019-06-26 23:42:56 -0400

    update questions

---
+++ remove_duplicates.cpp
@@ -0,0 +1,27 @@
+// Remove duplicates from a list or Generate all of the permutations of a string.
+
+#include <iostream>
+#include <string>
+#include <algorithm> 
+using std::cout;
+using std::string;
+
+// edge case: str  = 0 characters return
+// range of characters
+// empty string error
+
+// 1. sort it
+// 2. for loop if arr[i] == arr[i+]
+
+string Remove(string str){
+
+  std::sort(str.begin(), str.end());
+
+  for (int i = 0; i < str.length(); i++){
+
+    i
+    
+  }
+  
+  
+}
