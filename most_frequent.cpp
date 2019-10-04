44000762e02947977836f83a303a7eea0c83c7ec
muskankapoor <muskankapoor016@gmail.com>
2019-10-03 22:57:23 -0400

    update

---
+++ most_frequent.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int mostFrequent(int arr[], int size){
+
+
+  unordered_map<int, int> hash;
+
+  for (int i = 0; i < size; i++){
+    hash[arr[i]]++;
+  }
+
+  int max_count = 0, res = -1; 
+
+  for (auto i : hash) { 
+        if (max_count < i.second) { 
+            res = i.first; 
+            max_count = i.second; 
+        } 
+    } 
+  
+    return res; 
+} 
+  
+  
+
+
+
+int main() 
+{ 
+    int arr[] = { 1, 5, 2, 1, 3, 2, 1 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    cout << mostFrequent(arr, n); 
+    return 0; 
+} 
