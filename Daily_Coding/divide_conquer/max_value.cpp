cfeb8a5f87234158465f40e034ae2a08070a4418
Muskan Kapoor <muskankapoor016@gmail.com>
2020-02-04 14:58:59 -0500

    update

---
+++ Daily_Coding/divide_conquer/max_value.cpp
@@ -0,0 +1,43 @@
+#inlcude <Iostream>
+
+int maxvalue(int numbers[], int arrayszie){
+
+  if (arraysize == 1){
+    return numbers[0];
+  }
+
+  else{
+    // process array that is one less - 
+    int max = maxvalue(numbers, arrayszie - 1);
+    // compare last elemetn and max element 
+    if (max > numbers[arrayszie - 1]){
+      return max; 
+    }
+    else{
+      return numbers[arrayszie - 1];
+    }
+    
+  }
+
+}
+
+
+// divide into half
+int maxvalueDC(int numbers[], int start, int end){
+  if (start == end){
+    return numbers[start];
+  }
+  else{
+    int halfway = (start + end)/2; // mid way
+    int left = maxvalueDC(numbers, start, halfway);
+    int right = maxvalueDC(numbers, halfway + 1, end);
+
+    if (left > right){
+      return left;
+    }
+    else{
+      return right;
+    }
+    
+  }
+}
--- Daily_Coding/dynamic_programmming/subset_sum.cpp
+++ Daily_Coding/dynamic_programmming/subset_sum.cpp
@@ -7,7 +7,54 @@
 // 1 7 8 10 k = 11 [1, 10]
 
 
-// Tushar video in youtube of subet sum
-// can check in blackstone book
-// https://www.youtube.com/watch?v=s6FhG--P7z0
 
+bool subsetSum(int input[], int total) {
+
+        boolean T[][] = new boolean[input.length + 1][total + 1];
+        for (int i = 0; i <= input.length; i++) {
+            T[i][0] = true;
+        }
+
+        for (int i = 1; i <= input.length; i++) {
+            for (int j = 1; j <= total; j++) {
+                if (j - input[i - 1] >= 0) {
+                    T[i][j] = T[i - 1][j] || T[i - 1][j - input[i - 1]];
+                } else {
+                    T[i][j] = T[i-1][j];
+                }
+            }
+        }
+        return T[input.length][total];
+
+    }
+
+    public boolean partition(int arr[]) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+        sum = sum / 2;
+        boolean[][] T = new boolean[arr.length + 1][sum + 1];
+
+        for (int i = 0; i <= arr.length; i++) {
+            T[i][0] = true;
+        }
+
+        for (int i = 1; i <= arr.length; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (j - arr[i - 1] >= 0) {
+                    T[i][j] = T[i - 1][j - arr[i - 1]] || T[i - 1][j];
+                } else {
+                    T[i][j] = T[i-1][j];
+                }
+            }
+        }
+        return T[arr.length][sum];
+    }
+
+
+}
