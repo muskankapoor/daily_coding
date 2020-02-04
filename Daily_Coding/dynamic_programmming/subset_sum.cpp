ac521cf2ff2c309e3f9eb462dcac7a115bc7ed5e
muskankapoor <muskankapoor016@gmail.com>
2020-02-04 12:04:31 -0500

    update

---
+++ Daily_Coding/dynamic_programmming/subset_sum.cpp
@@ -0,0 +1,13 @@
+// Given a list of integers S and a target number k, write a function that returns a subset of S that adds up to k. If such a subset cannot be made, then return null.
+
+// Integers can appear more than once in the list. You may assume all numbers in the list are positive.
+
+// For example, given S = [12, 1, 61, 5, 9, 2] and k = 24, return [12, 9, 2, 1] since it sums up to 24.
+
+// 1 7 8 10 k = 11 [1, 10]
+
+
+// Tushar video in youtube of subet sum
+// can check in blackstone book
+// https://www.youtube.com/watch?v=s6FhG--P7z0
+
