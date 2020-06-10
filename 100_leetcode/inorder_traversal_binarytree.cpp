981b4cccc51a65c92e97ac5fe69ee0c871a4b232
Muskan Kapoor <muskankapoor016@gmail.com>
2020-06-10 00:02:08 -0400

    update

---
+++ 100_leetcode/inorder_traversal_binarytree.cpp
@@ -0,0 +1,22 @@
+// Given a binary tree, return the inorder traversal of its nodes¡¯ values
+
+
+
+
+struct Tree{
+
+  int data;
+  Tree *left;
+  Tree *right; 
+
+};
+
+
+
+
+void inorder(Tree *root){
+
+  cout << *root;
+  int l
+
+}
--- 100_leetcode/positive_missing.cpp
+++ 100_leetcode/positive_missing.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 using namespace std;
 
 
 // Given an unsorted integer array, find the smallest missing positive integer.
 
+// algorhtmns:
+
+// 1. on2 search for 1 to n
+// 2. onlogn sorting
+// 3. hashing on time but on space
+
+// below is on time
+
+int SmallestMisingPositive(vector<int> arr){
+
+  int n = arr.size();
+  bool present[n + 1] = {false};
+  for (int i = 0; i < n; i++){
+    if (arr[i] > 0 && arr[i] <= n){
+      present[arr[i]] = true;
+      
+    }
+  }
+
+  for (int i = 1; i <= n; i++){
+    if (!present[i]){
+      return i; 
+    }
+  }
+  return n + 1;
+}
 
 
+
+int main(){
+
+
+  vector<int> arr = {7,8,9,11,12};
+  cout << SmallestMisingPositive(arr);
+
+
+
+}
